9599f6244ec841d4ce0bd1fba53cb0521d80ab64
Brofix <9402720+Blatts12@users.noreply.github.com>
2022-02-27 16:25:33 +0100

    preloading given points with user in show/1 PointsController

--- lib/good_deeds_web/controllers/points_controller.ex
+++ lib/good_deeds_web/controllers/points_controller.ex
@@ -3,7 +3,7 @@ defmodule GoodDeedsWeb.PointsController do
   alias GoodDeeds.Repo
 
   def show(%{assigns: %{current_user: user}} = conn, _params) do
-    user_with_points = user |> Repo.preload(points: [:given_points])
+    user_with_points = user |> Repo.preload(points: [given_points: [:user]])
 
     render(conn, "show.html", user_with_points: user_with_points)
   end
