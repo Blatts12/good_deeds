e45cb4670c2cea726ba28abe649246c23a8485de
Brofix <9402720+Blatts12@users.noreply.github.com>
2022-03-01 15:26:19 +0100

    giveaway_create now works

--- lib/good_deeds_web/controllers/points_controller.ex
+++ lib/good_deeds_web/controllers/points_controller.ex
@@ -1,9 +1,9 @@
 defmodule GoodDeedsWeb.PointsController do
   use GoodDeedsWeb, :controller
-  alias GoodDeeds.Repo
-  alias GoodDeeds.Points
-  alias GoodDeeds.Accounts
+  alias GoodDeeds.{Repo, Points, Accounts}
+  alias GoodDeeds.Points.GivenPoints
   import Ecto.Changeset
+  import Ecto
 
   def show(%{assigns: %{current_user: user}} = conn, _params) do
     %{points: points} = user |> Repo.preload(points: [given_points: [:user]])
@@ -38,8 +38,16 @@ defmodule GoodDeedsWeb.PointsController do
       {:ok, _giveaway} ->
         case check_user(from_user, giveaway["email"], changeset) do
           %Accounts.User{} = to_user ->
-            giveaway_points(from_user, to_user, giveaway["giveaway"])
-            redirect(conn, to: Routes.points_path(conn, :show))
+            case giveaway_points(from_user, to_user, giveaway["giveaway"]) do
+              {:ok, _result} ->
+                redirect(conn, to: Routes.points_path(conn, :show))
+
+              {:error, changeset} ->
+                render(conn, "giveaway_new.html",
+                  points: points,
+                  changeset: changeset
+                )
+            end
 
           %Ecto.Changeset{} = changeset ->
             changeset = %Ecto.Changeset{changeset | action: :create}
@@ -75,5 +83,20 @@ defmodule GoodDeedsWeb.PointsController do
   end
 
   defp giveaway_points(from_user, to_user, points) do
+    points = String.to_integer(points)
+    %{points: from_points} = Repo.preload(from_user, :points)
+    %{points: to_points} = Repo.preload(to_user, :points)
+
+    Points.update_user_points(from_points, %{pool: from_points.pool - points})
+    Points.update_user_points(to_points, %{points: to_points.points + points})
+
+    changeset =
+      from_points
+      |> build_assoc(:given_points)
+      |> change()
+      |> put_assoc(:user, to_user)
+      |> GivenPoints.changeset(%{given: points})
+
+    Repo.insert(changeset)
   end
 end
